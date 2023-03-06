b60ecef4438e18e9ec0efdac1b1cf03c86929777
mikaeltreppo <mikaeltreppo@outlook.com>
2023-03-06 14:28:48 +0100

    Aggiunto punteggio in game over

--- js/main.js
+++ js/main.js
@@ -3,17 +3,14 @@ let buttonDom = document.getElementById('buttonStartGame');
 let pointsDom = document.getElementById('points');
 let initializationDom = document.getElementById('initialization');
 let welcomeDom = document.getElementById('welcome');
-
 initializationDom.addEventListener('click', function () {
     entranceDom.classList.remove('d-none');
     welcomeDom.classList.add('d-none');
 })
-
 //array contenente numeri casuali pc
 let mineLocation = [];
 // creaziine numeri casuali computer
 let casualRandomNumber;
-
 for (let i = 0; i < 16; i++) {
     casualRandomNumber = createNumberPc(1, 100);
     function createNumberPc(min, max) {
@@ -27,35 +24,27 @@ for (let i = 0; i < 16; i++) {
         i--
     }
 }
-
-
 console.log(casualRandomNumber);
 console.log(mineLocation);
 //creo un event listener sull click per far si che se la casella con il n segnato ha lo stesso numero di array[i] diventa rossa e si ferma il gioco
-
 let points = 0
 pointsDom.innerHTML = points;
-
 function explosion(num, box) {
     for (let i = 0; i < mineLocation.length; i++) {
         if (num == parseInt(mineLocation[i])) {
             box.classList.add('bg_red');
-            alert('Game over!');
+            alert('Game over! Il tuo punteggio è ' + points);
             return;
         }
     }
     points++;
     pointsDom.innerHTML ="Punteggio: " + points;
 }
-
-
-
 //riparte il gioco
 buttonDom.addEventListener('click',
     function () {
         location.reload();
     });
-
 //creato numero all'interno e box e sfondo blu
 for (let i = 1; i <= 100; i++) {
     let boxCreated = createBox();
@@ -69,7 +58,6 @@ for (let i = 1; i <= 100; i++) {
             explosion(num, boxCreated)
         })
 }
-
 //funzione creare caselle
 function createBox() {
     let newBox = document.createElement('div')
